d23d62c994ce7bdee784ea82f678a78a26047645
ogurasousui <ogurasousui2@gmail.com>
2017-10-17 13:42:19 +0900

    テストを追加してみる 一部テストがうまく動きません

---
+++ src/redux/counter/__tests__/Counter.spec.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import {Counter} from '../Counter'
+import {shallow} from 'enzyme'
+import {CounterState} from '../module'
+import {ActionDispatcher} from '../Container'
+
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Counter', () => {
+
+    it('rendering', () => {
+        const actions:any = {}
+        const state: CounterState = {num: 1}
+        const wrapper = shallow(<Counter value={state} actions={actions} />)
+        // TODO このテストがうまく通らない
+        expect(wrapper.find('p').at(0).prop('children')).toBe('score: 1')
+    })
+
+    it('click', () => {
+        const actionSpy = new ActionDispatcher(null!)
+        spyOn(actionSpy, 'increment')
+        const state: CounterState = {num: 0}
+        const wrapper = shallow(<Counter value={state} actions={actionSpy} />)
+        wrapper.find('button').at(0).simulate('click')
+        expect(actionSpy.increment).toHaveBeenCalledWith(3)
+    })
+})
