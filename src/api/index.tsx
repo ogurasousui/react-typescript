1b355e52e137b213a84225093cbdafc16ff2b1e3
ogurasousui <ogurasousui2@gmail.com>
2017-10-31 19:19:35 +0900

    マテリアルデザインをやろうとした

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -12,6 +12,7 @@ import { Provider } from 'react-redux'
 import Main from "./components/Main"
 
 import reducers from './reducers/index'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 let store = createStore(
     reducers,
---
+++ src/api/util/Api.ts
@@ -0,0 +1,5 @@
+export default class Api {
+    fetch (url: string): any {
+        return {}
+    }
+}
